9ced5cbb276b0d115151ac67acccef0f2a935cde
Kurt Sarmiento <sarmijustine@gmail.com>
2025-10-12 23:11:21 +0800

    Update database connection credentials for production environment

--- db_config.php
+++ db_config.php
@@ -1,10 +1,10 @@
 <?php
 // Define database credentials
 // NOTE: These are default XAMPP settings. If you changed your MySQL password, update it here.
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', ''); 
-define('DB_NAME', 'auf_requests_db');
+define('DB_SERVER', 'sql104.infinityfree.com');
+define('DB_USERNAME', 'if0_40151522');
+define('DB_PASSWORD', '4AQrSjzpnJt5xl'); 
+define('DB_NAME', 'if0_40151522_aufrequestssystem');
 
 // Attempt to connect to MySQL database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
